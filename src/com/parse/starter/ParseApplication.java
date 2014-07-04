05231a51ee6059fb1925c18a491710e548b70278
Steve Paik <spaik@google.com>
2014-07-03 22:07:33 -0700

    Initial Parse SDK

---
+++ src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,36 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		// Add your initialization code here
+		Parse.initialize(this, "ZnS9KINPID8w6NcMT25IIcYRB6xVD0ELOZsDYcH1",
+				"0m2t7SvIxJijmnm312SxI7pTGcp6s0wr973CltoG");
+
+
+
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+	    
+		// If you would like all objects to be private by default, remove this line.
+		defaultACL.setPublicReadAccess(true);
+		
+		ParseACL.setDefaultACL(defaultACL, true);
+		
+		ParseObject testObject = new ParseObject("TestObject");
+		testObject.put("foo", "bar");
+		testObject.saveInBackground();
+
+	}
+
+}
---
+++ src/com/parse/starter/ParseStarterProjectActivity.java
@@ -0,0 +1,19 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+import com.parse.R;
+
+public class ParseStarterProjectActivity extends Activity {
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.main);
+		ParseAnalytics.trackAppOpened(getIntent());
+		
+	}
+}
