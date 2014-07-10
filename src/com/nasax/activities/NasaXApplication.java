3d165366e3801717fcbb189e517b86180b933f44
Steve Paik <spaik@google.com>
2014-07-10 00:47:16 -0700

    PUser is broken, but I can log some User data

--- src/com/nasax/activities/NasaXApplication.java
+++ src/com/nasax/activities/NasaXApplication.java
@@ -1,13 +1,17 @@
 package com.nasax.activities;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.nasax.models.Event;
+import com.nasax.models.EventUser;
+import com.nasax.models.PUser;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.parse.Parse;
 import com.parse.ParseACL;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
@@ -27,6 +31,8 @@ public class NasaXApplication extends Application {
 
 		// Add your initialization code here
 	    ParseObject.registerSubclass(Event.class);
+	    ParseObject.registerSubclass(EventUser.class);
+	    ParseUser.registerSubclass(PUser.class);
 	    Parse.enableLocalDatastore(this);
 		Parse.initialize(this, "ZnS9KINPID8w6NcMT25IIcYRB6xVD0ELOZsDYcH1",
 				"0m2t7SvIxJijmnm312SxI7pTGcp6s0wr973CltoG");
@@ -42,5 +48,16 @@ public class NasaXApplication extends Application {
 		ParseObject testObject = new ParseObject("TestObject");
 		testObject.put("foo", "bar");
 		testObject.saveInBackground();
+		
+		// Print some debug info
+		ParseUser me = ParseUser.getCurrentUser();
+		try {
+			me.fetch();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Log.d("debug", "Your username is: " + me.getUsername());
+		Log.d("debug", "Your name is: " + me.getString("name"));
 	}
 }
