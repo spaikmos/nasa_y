e5b66bb7f6f4fcd8b34c9a90b276866fe117bd27
Steve Paik <spaik@google.com>
2014-07-11 22:59:25 -0700

    Completed event profile activity

--- src/com/nasax/activities/EventProfileActivity.java
+++ src/com/nasax/activities/EventProfileActivity.java
@@ -1,13 +1,14 @@
 package com.nasax.activities;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.nasax.fragments.ProfileAclFragment;
-import com.nasax.models.Event;
 import com.nasax.models.EventUser;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -16,19 +17,14 @@ import com.parse.ParseUser;
 
 public class EventProfileActivity extends FragmentActivity {
 	private EventUser eventUser;
-	private ParseUser me;
-	private Event event;
+	private EditText etNote;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_event_profile);
 		String eventUserId = getIntent().getStringExtra("eventUserId");
-		me = ParseUser.getCurrentUser();
 
-		Log.d("debug", "eventUserId = " + eventUserId);
-		Log.d("debug", "screenname = " + me.getUsername());
-		
 		// Get the eventUser data from local datastore
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("EventUser");
 		query.fromLocalDatastore();
@@ -38,50 +34,40 @@ public class EventProfileActivity extends FragmentActivity {
 			e.printStackTrace();
 		}
 		
-		Log.d("debug", "eventUser = " + eventUser.toString());
-
-		// Get the event data from local datastore
-		query = ParseQuery.getQuery("event");
-		query.fromLocalDatastore();
-		try {
-			event = (Event)query.get(eventUser.getEventId());
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-
 		// Modify the action bar title
 		ActionBar actionBar = this.getActionBar();
 		actionBar.setTitle("Profile Access");
 		
 		// Populate textviews with user data
 		TextView tvUsername = (TextView) findViewById(R.id.tvUsername);
-		tvUsername.setText(me.getUsername());
+		tvUsername.setText(ParseUser.getCurrentUser().getUsername());
 		
 		// Setup fragments
 		setupFragment(R.id.fName, "showName", "name", "Name");
+		setupFragment(R.id.fEmail, "showEmail", "email", "E-mail");
+		setupFragment(R.id.fAddress, "showAddress", "address", "Address");
+		setupFragment(R.id.fSchoolName, "showSchoolName", "schoolName", "School Name");
+		setupFragment(R.id.fCompanyName, "showCompanyName", "companyName", "Company Name");
+		setupFragment(R.id.fOccupation, "showOccupation", "occupation", "Occupation");
+		setupFragment(R.id.fAbout, "showAbout", "about", "About Me");
 		
-		/*
-		tvName = (TextView) findViewById(R.id.tvName);
-		tvName.setText(me.getString("name"));
-		tvEmail = (TextView) findViewById(R.id.tvEmail);
-		tvEmail.setText(me.getEmail());
-		tvAddress = (TextView) findViewById(R.id.tvAddress);
-		tvAddress.setText(me.getString("address"));
-		tvSchoolName = (TextView) findViewById(R.id.tvSchoolName);
-		tvSchoolName.setText(me.getString("schoolName"));
-		tvCompanyName = (TextView) findViewById(R.id.tvCompanyName);
-		tvCompanyName.setText(me.getString("companyName"));
-		tvOccupation = (TextView) findViewById(R.id.tvOccupation);
-		tvOccupation.setText(me.getString("occupation"));
-		tvAbout = (TextView) findViewById(R.id.tvAbout);
-		tvAbout.setText(me.getString("about"));
-		// Populate the switches
-		Boolean val = eventUser.getShowAbout();
-*/
+		// Setup the Edit Text Note
+		etNote = (EditText) findViewById(R.id.etNote);
+		etNote.setText(eventUser.getString("note"));
 	}
 	
-	void setupFragment(Integer rId, String eUCol, String uCol, String swText) {
+	private void setupFragment(Integer rId, String eUCol, String uCol, String swText) {
 		ProfileAclFragment proAclFrag = (ProfileAclFragment) getSupportFragmentManager().findFragmentById(rId);
 		proAclFrag.setupView(eventUser.getObjectId(), eUCol, uCol, swText);
 	}
+	
+    public void onSaveButtonClick(View v) {
+    	eventUser.put("note",  etNote.getText().toString());
+    	eventUser.saveInBackground();
+    }
+
+    public void onClearButtonClick(View v) {
+    	etNote.setText(null);
+    	onSaveButtonClick(v);
+    }
 }
