5f3323b434169b936c1719b48cd12fe53fab84df
Steve Paik <spaik@google.com>
2014-07-19 23:48:43 -0700

    Implement QR code flow - Scanning a QR code enables user to join an event - Removed all instances of requestFocus to prevent keyboard from popping up - Added color style sheet to create consistent theme

--- src/com/nasax/activities/MainActivity.java
+++ src/com/nasax/activities/MainActivity.java
@@ -1,25 +1,38 @@
 package com.nasax.activities;
 
+import java.util.List;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.nasax.models.Event;
+import com.nasax.models.EventUser;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import eu.livotov.zxscan.ZXScanHelper;
 
 public class MainActivity extends FragmentActivity {
 	static final int ZXSCAN_CODE = 12345;
-	
+	Context context;
+
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		ParseAnalytics.trackAppOpened(getIntent());
+		context = this;
 	}
 
 	@Override
@@ -34,6 +47,7 @@ public class MainActivity extends FragmentActivity {
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 		case R.id.miAddEvent:
+			ZXScanHelper.setCustomScanLayout(R.layout.qr_scanner);
 			ZXScanHelper.scan(this, ZXSCAN_CODE);
 			return true;
 		case R.id.miProfile:
@@ -52,9 +66,44 @@ public class MainActivity extends FragmentActivity {
 
 	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
 		if (resultCode == RESULT_OK && requestCode == ZXSCAN_CODE) {
-			String scannedCode = ZXScanHelper.getScannedCode(data);
-			Toast.makeText(this,  "Scan = " + scannedCode, Toast.LENGTH_LONG).show();
+			final String eventId = ZXScanHelper.getScannedCode(data);
+			
+			Log.d("debug", "QR code scan = " + eventId);
+			// Get the event object
+			ParseQuery<ParseObject> query = ParseQuery.getQuery("event");
+			query.getInBackground(eventId, new GetCallback<ParseObject>() {
+				public void done(ParseObject object, ParseException e) {
+			        if (e == null) {
+			        	// This is a valid event.  Check if user is already in this event
+			        	final Event event = (Event)object;
+			        	ParseQuery<ParseObject> query = ParseQuery.getQuery("EventUser");
+			     		query.whereEqualTo("event",  event);
+			     		query.whereEqualTo("user", ParseUser.getCurrentUser());
+			     		query.findInBackground(new FindCallback<ParseObject>() {
+							public void done(List<ParseObject> objects,
+									ParseException e) {
+								if (e == null) {
+									if(objects.size() > 0) {
+										// User is already bound to this event.
+										Toast.makeText(context,  "Already invited to " + event.getEventName(), Toast.LENGTH_LONG).show();
+									} else {
+										// Create a new entry in the table for this user
+										EventUser eventUser = new EventUser();
+										eventUser.put("event", event);
+										eventUser.put("user",  ParseUser.getCurrentUser());
+										eventUser.saveInBackground();
+										Toast.makeText(context,  "Added " + event.getEventName(), Toast.LENGTH_LONG).show();
+									}
+								} else {
+									e.printStackTrace();
+								}
+							}			     			
+			     		});
+			        } else {
+			            Toast.makeText(context, "Invalid EventId = '" + eventId, Toast.LENGTH_LONG).show();
+			        }
+			    }
+			});
 		}
 	}
-
 }
