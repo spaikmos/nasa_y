e8abf97fcdf12b8899647dc106de5f296a5d9109
Steve Paik <spaik@google.com>
2014-07-24 03:20:47 -0700

    Styling changes - Remove top text from login activity - Center text in event list - Cleanup event details

--- src/com/nasax/fragments/AttendeeListFragment.java
+++ src/com/nasax/fragments/AttendeeListFragment.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.nasax.activities.EventActivity.OnKeyEventListener;
 import com.nasax.activities.R;
